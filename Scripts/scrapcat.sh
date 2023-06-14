6b800340a935a72760ca67ec9c98e023033734b5
rickeymandraque <rickeymandraque@users.noreply.github.com>
2023-06-15 00:18:50 +0200

    Update scrapcat 2.1.7
    - Utilisation d'une syntaxe de déclaration des fonctions plus récente
    -  Utilisation de fonctions internes pour extraire les informations des blocs de données
    - Utilisation des commandes curl et grep avec des options pour extraire les informations des blocs de données
    - Utilisation de paramètres de fonction pour passer les informations extraites
    - Ajout de fonctions supplémentaires pour nettoyer et gérer l'interruption du script
    - Amélioration de la lisibilité du code avec des noms de fonction explicites
    - Suppression de variables inutiles
    - Amélioration de la gestion des erreurs avec des messages d'erreur et un nettoyage approprié en cas d'interruption du script

--- Scripts/scrapcat.sh
+++ Scripts/scrapcat.sh
@@ -15,16 +15,54 @@
 # Créer un répertoire temporaire dans "tmpfs"
 tmpdir=$(mktemp -d -p /dev/shm)
 
-# Tableau contenant les URL des pays
-countries=("france" "undefined")
-
-# Fonction pour extraire le contenu entre les balises <span>
-extract_span_content() {
-    awk -F'</span>' '{print $1}' | awk 'NF'
+# Fonction pour nettoyer et sortir en cas d'interruption du script
+function cleanup() {
+    echo ""
+    echo "Script interrompu par l'utilisateur"
+    sleep 1
+    echo "Nettoyage en cours"
+    rm -r "$tmpdir"
+    sleep 1
+    echo "Terminé"
+    sleep 1
+    exit 1
 }
 
+# Gérer l'interruption du script en cas de pression de la touche CTRL+C
+trap cleanup SIGINT
+
+# Fonction pour extraire le contenu entre les balises <span>
+function extract_span_content() {
+    awk -F'</span>' '{print $1}' | awk 'NF'
+}
+function cat_block() {
+    cat "$block_file"
+}
+
+function get_country_list() {
+    local html
+    html=$(curl -sL "https://iptvcat.com")
+
+    mapfile -t countries < <(echo "$html" | awk -F'"|</option>' '/<option value="/ && !/all/ {gsub(/ /, "_", $2); print $2}')
+
+    echo "${countries[@]}"
+}
+
+# Function to print country list
+function print_country_list() {
+    local countries=("$@")
+    echo "Les pays suivants vont être analysés :"
+    for country in "${countries[@]}"; do
+        country=$(echo "$country" | sed 's/_/ /g')
+        echo -e "$country"
+    done
+}
+
+curl_cmd="curl -sL"
+grep_cmd="grep -E"
+
 # Fonction pour extraire les informations d'un bloc de données
-extract_info() {
+function extract_info() {
     local block_file="$1"
     local checked
     local liveliness
@@ -38,47 +76,83 @@ extract_info() {
     local stream_link
 
     # Extraire les informations supplémentaires du bloc
-    checked=$(cat "$block_file" | awk -F'<span class='\''titile_span checked_title'\''>Checked: </span><span class='\''minor_content'\''>' '{print $2}' | extract_span_content)
-    liveliness=$(cat "$block_file" | sed -n "s/.*background-color: rgba([^)]*)'>\([0-9]\+\)<.*/\1/p")
-    days=$(cat "$block_file" | awk -F'<span class='\''titile_span checked_title ml-15'\'' style='\''width: 40px'\''>Days: </span><span>' '{print $2}' | extract_span_content)
-    status=$(cat "$block_file" | awk -F'<div class='\''state span ' '{print $2}' | awk -F"'" '{print $1}' | awk 'NF')
-    # sed remplace les virgules par des retour à la ligne, à modifier si exportation vers json.
-    formats=$(cat "$block_file" | awk -F'<span class='\''titile_span formats'\''>Formats: </span><span class='\''minor_content'\''>' '{print $2}' | extract_span_content | sed 's/, /\n/g')
-    country=$(cat "$block_file" | awk -F'<span class='\''titile_span_small'\''>Country: </span><span class='\''minor_content_server'\''>' '{print $2}' | extract_span_content)
-    region=$(cat "$block_file" | awk -F'<span class='\''titile_span_small'\''>Region: </span><span class='\''minor_content_server'\''>' '{print $2}' | extract_span_content)
-    city=$(cat "$block_file" | awk -F'<span class='\''titile_span_small'\''>City: </span><span class='\''minor_content_server'\''>' '{print $2}' | extract_span_content)
-    channel_title=$(cat "$block_file" | awk -F'title="' '/title="[^"]+">([^<]+)<\/span>/ {match($0, /title="[^"]+">([^<]+)<\/span>/, title); if (title[1] && !/Last checked/) print title[1]}' | sed -E 's/[0-9]{3,},\s\.{3}//')
-    stream_link=$(cat "$block_file" | grep -oP '<a href="\K[^"]+' | awk 'NR==1')
-    real_link=$(curl -sL $stream_link | grep -E '^https?'| sed 's/[?&]checkedby:iptvcat\.com$//')
+    checked=$(cat_block | awk -F'<span class='\''titile_span checked_title'\''>Checked: </span><span class='\''minor_content'\''>' '{print $2}' | extract_span_content)
+    liveliness=$(cat_block | sed -n "s/.*background-color: rgba([^)]*)'>\([0-9]\+\)<.*/\1/p")
+    days=$(cat_block | awk -F'<span class='\''titile_span checked_title ml-15'\'' style='\''width: 40px'\''>Days: </span><span>' '{print $2}' | extract_span_content)
+    status=$(cat_block | awk -F'<div class='\''state span ' '{print $2}' | awk -F"'" '{print $1}' | awk 'NF')
+    formats=$(cat_block | awk -F'<span class='\''titile_span formats'\''>Formats: </span><span class='\''minor_content'\''>' '{print $2}' | extract_span_content | sed 's/, /\n/g')
+    country=$(cat_block | awk -F'<span class='\''titile_span_small'\''>Country: </span><span class='\''minor_content_server'\''>' '{print $2}' | extract_span_content)
+    region=$(cat_block | awk -F'<span class='\''titile_span_small'\''>Region: </span><span class='\''minor_content_server'\''>' '{print $2}' | extract_span_content)
+    city=$(cat_block | awk -F'<span class='\''titile_span_small'\''>City: </span><span class='\''minor_content_server'\''>' '{print $2}' | extract_span_content)
+    channel_title=$(cat_block | awk -F'title="' '/title="[^"]+">([^<]+)<\/span>/ {match($0, /title="[^"]+">([^<]+)<\/span>/, title); if (title[1] && !/Last checked/) print title[1]}' | sed -E 's/[0-9]{3,},\s\.{3}//')
+    stream_link=$(cat_block | grep -oP '<a href="\K[^"]+' | awk 'NR==1')
+    real_link=$($curl_cmd "$stream_link" | $grep_cmd '^https?'| sed 's/[?&]checkedby:iptvcat\.com$//')
 
-    # Afficher les informations supplémentaires extraites
+    display_info "$channel_title" "$status" "$checked" "$liveliness" "$days" "$formats" "$country" "$region" "$city" "$stream_link" "$real_link"
+}
+
+# Fonction pour afficher les informations supplémentaires extraites
+function display_info() {
     echo "======================================================================================="
-    echo "Titre de la chaîne : $channel_title"
-    echo "Date de dernière vérification : $checked"
-    echo "Pourcentage de liveliness : $liveliness"
-    echo "Durée de vie (en jours) : $days"
-    echo "Statut : $status"
-    echo "Formats :"
-    echo "$formats"
-    echo "Pays de diffusion : $country"
-    echo "Région de diffusion : $region"
-    echo "Ville de diffusion : $city"
-    echo "Lien du flux : $stream_link"
-    echo "Lien réél : $real_link"
+
+    # Parcourir tous les paramètres
+    for param in "$@"; do
+        case $param in
+            "$channel_title")
+                echo "Titre de la chaîne : $channel_title"
+                ;;
+            "$checked")
+                echo "Date de dernière vérification : $checked"
+                ;;
+            "$liveliness")
+                echo "Pourcentage de liveliness : $liveliness"
+                ;;
+            "$days")
+                echo "Durée de vie (en jours) : $days"
+                ;;
+            "$status")
+                echo "Statut : $status"
+                ;;
+            "$formats")
+                echo "Formats :"
+                echo "$formats"
+                ;;
+            "$country")
+                echo "Pays de diffusion : $country"
+                ;;
+            "$region")
+                echo "Région de diffusion : $region"
+                ;;
+            "$city")
+                echo "Ville de diffusion : $city"
+                ;;
+            "$stream_link")
+                echo "Lien du flux : $stream_link"
+                ;;
+            "$real_link")
+                echo "Lien réél : $real_link"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
     echo "======================================================================================="
 }
 
 # Fonction pour parcourir une page et extraire les informations des blocs
-scrape_page() {
+function scrape_page() {
     local page_url="$1"
-    local html=$(curl -sL "$page_url")
+    local html
+    html=$($curl_cmd "$page_url")
     local filtered_html
 
     # Filtre pour supprimer le contenu avant et après les blocs souhaités
     filtered_html=$(echo "$html" | awk '/<tbody class="streams_table">/,/<!-- IptvCat_notice_up -->/')
 
     # Extraire les blocs de données souhaités à partir du HTML filtré
-    local blocks=$(echo "$filtered_html" | awk '/<div class='\''popover_channel_holder'\''>/{block=$0; while(getline > 0){block=block"\n"$0; if($0 ~ /data-content="/) break} print block; print "===================================="}')
+    local blocks
+    blocks=$(echo "$filtered_html" | awk '/<div class='\''popover_channel_holder'\''>/{block=$0; while(getline > 0){block=block"\n"$0; if($0 ~ /data-content="/) break} print block; print "===================================="}')
 
     # Parcourir les blocs et extraire les informations supplémentaires
     while IFS= read -r line; do
@@ -90,29 +164,74 @@ scrape_page() {
     done <<< "$blocks"
 }
 
+# Fonction pour remplacer les espaces par des underscore dans les URL des pays
+function replace_spaces_with_underscore() {
+    local country_url="$1"
+    local replaced_url="${country_url// /_}"
+    echo "$replaced_url"
+}
+
 # Parcourir les URL des pays
-for country in "${countries[@]}"; do
+if [[ "$1" == "--all-countries" || "$1" == "-ac" ]]; then
+    countries=($(get_country_list))
+    echo "ATTENTION : Tous les pays vont être parcourus !"
+    print_country_list "${countries[@]}"
+    echo "Appuyer sur CTRL+C pour annuler"
+    sleep 10
+    shift
+else
+    countries=("france" "undefined")
+fi
+
+# Fonction pour vérifier si un pays est sur la liste noire (blacklist)
+function is_blacklisted() {
+    local blacklist=("africa")  # Liste noire (blacklist)
+    local country="$1"
+    for blacklisted_country in "${blacklist[@]}"; do
+        if [[ "$country" == "$blacklisted_country" ]]; then
+            return 0  # Le pays est sur la liste noire
+        fi
+    done
+    return 1  # Le pays n'est pas sur la liste noire
+}
+
+index=0
+while [[ $index -lt ${#countries[@]} ]]; do
+    country="${countries[index]}"
+    if is_blacklisted "$country"; then
+        echo "Le pays '$country' est sur la liste noire. Passage au pays suivant..."
+        index=$((index + 1))
+        continue
+    fi
     page_number=1
-    page_url="https://iptvcat.com/$country"
+    country_url=$(replace_spaces_with_underscore "https://iptvcat.com/$country")
+    page_url="$country_url"
 
     # Récupérer les informations pour toutes les pages disponibles
     while true; do
         echo "Scraping $page_url (Page $page_number)"
+        echo "Pays en cours : $country"
         scrape_page "$page_url"
 
         # Vérifier si "Nothing found!" est présent dans la page
-        if curl -sL "$page_url" | grep -q "Nothing found!"; then
+        if $curl_cmd "$page_url" | grep -q "Nothing found!"; then
             break
         fi
 
         echo "CHANGEMENT DE PAGE !"
         sleep 2
-
         page_number=$((page_number + 1))
-        page_url="https://iptvcat.com/$country/$page_number"
+        page_url="$country_url/$page_number"
     done
 
-    echo "Aucune page supplémentaire à analyser pour le pays $country."
+    index=$((index + 1))
+    echo "CHANGEMENT DE PAYS !"
+    sleep 3
+
+    # Réinitialiser les variables pour le nouveau pays
+    page_number=1
+    country_url=$(replace_spaces_with_underscore "https://iptvcat.com/${countries[index]}")
+    page_url="$country_url"
 done
 
 # Supprimer le répertoire temporaire
